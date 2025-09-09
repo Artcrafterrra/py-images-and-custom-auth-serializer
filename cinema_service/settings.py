d3470354d963c0f1e23b47c9bf2d9351fddea032
Artcrafterrra <if.artcraft@gmail.com>
2025-09-09 19:23:03 +0300

    fix

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import importlib.util
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,7 +156,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
-import importlib.util
 
 DEBUG_TOOLBAR_AVAILABLE = (
     importlib.util.find_spec("debug_toolbar") is not None
